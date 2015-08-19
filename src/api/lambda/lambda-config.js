ceb7e8b5098f03a5c944da171802695bac60822b
scott coates <scoarescoare@gmail.com>
2015-08-19 18:30:35 -0500

    init!

---
+++ src/api/lambda/lambda-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  accessKeyId: 'abc',
+  secretAccessKey: '123',
+  region: 'us-west-2',
+  handler: 'index.handler',
+  role: '123',
+  functionName: 'hermes-qa-create-user',
+  timeout: 10,
+  memorySize: 128
+};
